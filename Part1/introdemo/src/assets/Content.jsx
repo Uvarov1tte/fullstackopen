c771426c332367ed45e2b0fb575fe062302e3ef0
Uvarov1tte <ngqchia@gmail.com>
2025-05-03 19:06:30 +0200

    1.5 completed

--- Part1/introdemo/src/assets/Content.jsx
+++ Part1/introdemo/src/assets/Content.jsx
@@ -1,11 +1,12 @@
 import { Part } from "./Part"
 
 export function Content(props) {
+    const parts = props.parts
     return (
         <>
-            <Part part={props.p1} exercise={props.e1}></Part >
-            <Part part={props.p2} exercise={props.e2}></Part >
-            <Part part={props.p3} exercise={props.e3}></Part >
+            <Part part={parts[0].name} exercise={parts[0].exercises}></Part >
+            <Part part={parts[1].name} exercise={parts[1].exercises}></Part >
+            <Part part={parts[2].name} exercise={parts[2].exercises}></Part >
         </>
     )
 }
--- Part1/introdemo/src/assets/Total.jsx
+++ Part1/introdemo/src/assets/Total.jsx
@@ -1,5 +1,9 @@
 export function Total(props) {
+    let total = 0
+    for (let i of props.parts) {
+        total += i.exercises
+    }
     return (
-        <div>Number of exercises {props.total}</div>
+        <div>Number of exercises {total}</div>
     )
 }
