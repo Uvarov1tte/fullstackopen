4b0de808d573d5a8dfc015804b22d61420964ed8
Nguyen <ngqchia@gmail.com>
2025-05-13 16:09:30 +0200

    4.18

--- Part4/bloglist/app.js
+++ Part4/bloglist/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const app = express()
 
@@ -16,6 +17,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
