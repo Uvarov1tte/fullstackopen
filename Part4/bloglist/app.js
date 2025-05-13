da363f224a38b016577f0003a5ca1f0596344941
Nguyen <ngqchia@gmail.com>
2025-05-13 14:19:04 +0200

    4.16

--- Part4/bloglist/app.js
+++ Part4/bloglist/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
 const app = express()
 
 const mongoUrl = process.env.NODE_ENV === 'test'
@@ -11,7 +12,11 @@ const mongoUrl = process.env.NODE_ENV === 'test'
 mongoose.connect(mongoUrl)
 
 app.use(express.json())
+app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- Part4/bloglist/controllers/blogs.js
+++ Part4/bloglist/controllers/blogs.js
@@ -7,23 +7,37 @@ blogsRouter.get('/', async (req, res) => {
     res.json(blogs)
 })
 
-blogsRouter.post('/', async (req, res) => {
+blogsRouter.get('/:id', async (req, res, next) => {
     try {
-        const blog = new Blog(req.body)
-        const user = await User.findById(body.userId)
-
-        if (!user) {
-            return response.status(400).json({ error: 'userId missing or not valid' })
+        const blog = await Blog.findById(req.params.id)
+        if (blog) {
+            res.json(blog)
+        } else {
+            res.status(404).end()
         }
+    } catch (err) {
+        next(err)
+    }
+})
+
+blogsRouter.post('/', async (req, res, next) => {
+
+    const blog = new Blog(req.body)
+    const user = await User.findById(body.userId)
+
+    if (!user) {
+        return response.status(400).json({ error: 'userId missing or not valid' })
+    }
+    try {
         const result = await blog.save()
         res.status(201).json(result)
         if (result) { }
     } catch (err) {
-        res.status(400).end()
+        next(err)
     }
 })
 
-blogsRouter.put('/:id', async (req, res) => {
+blogsRouter.put('/:id', async (req, res, next) => {
     const body = req.body
 
     const blog = {
@@ -32,14 +46,21 @@ blogsRouter.put('/:id', async (req, res) => {
         url: body.url,
         likes: body.likes,
     }
-
-    const updatedblog = await Blog.findByIdAndUpdate(req.params.id, blog, { new: true })
-    res.json(updatedblog)
+    try {
+        const updatedblog = await Blog.findByIdAndUpdate(req.params.id, blog, { new: true })
+        res.json(updatedblog)
+    } catch (err) {
+        next(err)
+    }
 })
 
-blogsRouter.delete('/:id', async (req, res) => {
-    await Blog.findByIdAndDelete(req.params.id)
-    res.status(204).end()
+blogsRouter.delete('/:id', async (req, res, next) => {
+    try {
+        await Blog.findByIdAndDelete(req.params.id)
+        res.status(204).end()
+    } catch (err) {
+        next(err)
+    }
 })
 
 module.exports = blogsRouter
