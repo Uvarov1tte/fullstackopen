8ea596d71198001862ae041b6f1f0a527e8cb2aa
Uvarov1tte <ngqchia@gmail.com>
2025-05-20 14:15:13 +0200

    6.9

--- Part6/anecdotes-redux/src/App.jsx
+++ Part6/anecdotes-redux/src/App.jsx
@@ -2,11 +2,13 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from './reducers/anecdoteReducer'
 import NewAnecdote from './components/NewAnecdote'
 import AnecdoteList from './components/AnecdoteList'
+import Filter from './components/Filter'
 
 const App = () => {
     return (
         <div>
             <h2>Anecdotes</h2>
+            <Filter/>
             <AnecdoteList/>
             <NewAnecdote/>
         </div>
