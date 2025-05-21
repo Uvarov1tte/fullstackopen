41a12f5bd3a17415f59d184c8e9265cefdd066bb
Nguyen <ngqchia@gmail.com>
2025-05-21 16:14:49 +0200

    6.22 draft

--- Part6/query-anecdotes/src/App.jsx
+++ Part6/query-anecdotes/src/App.jsx
@@ -1,20 +1,37 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from './services/requests'
 
 const App = () => {
+    const queryClient = useQueryClient()
+
+    const updateAnecdoteMutation = useMutation({
+        mutationFn: updateAnecdote,
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+        }
+    })
 
     const handleVote = (anecdote) => {
-        console.log('vote')
+        updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
     }
 
-    const anecdotes = [
-        {
-            "content": "If it hurts, do it more often",
-            "id": "47145",
-            "votes": 0
-        },
-    ]
+    const result = useQuery({
+        queryKey: ['anecdotes'],
+        queryFn: getAnecdotes
+    })
+    console.log(JSON.parse(JSON.stringify(result)))
 
+    if (result.isLoading) {
+        return <div>loading data...</div>
+    }
+
+    if (result.isError) {
+        return <div>anecdote servide not available due to problems in server</div>
+    }
+
+    const anecdotes = result.data
     return (
         <div>
             <h3>Anecdote app</h3>
