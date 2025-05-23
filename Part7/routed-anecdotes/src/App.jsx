d403d4951828cc5d912ea7d77e8ba401d33bd865
Uvarov1tte <ngqchia@gmail.com>
2025-05-23 21:04:14 +0200

    7.4

--- Part7/routed-anecdotes/src/App.jsx
+++ Part7/routed-anecdotes/src/App.jsx
@@ -11,6 +11,7 @@ import {
     useNavigate,
     useMatch
 } from "react-router-dom"
+import { useField } from './hooks/useField'
 
 
 const Menu = () => {
@@ -67,18 +68,18 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
-    const [info, setInfo] = useState('')
+    const content = useField('text')
+    const author = useField('text')
+    const info = useField('text')
 
     const navigate = useNavigate()
 
     const handleSubmit = (e) => {
         e.preventDefault()
         props.addNew({
-            content,
-            author,
-            info,
+            content: content.value,
+            author: author.value,
+            info: info.value,
             votes: 0
         })
         navigate('/')
@@ -90,15 +91,15 @@ const CreateNew = (props) => {
             <form onSubmit={handleSubmit}>
                 <div>
                     content
-                    <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+                    <input name='content' value={content.value} onChange={content.onChange} type={content.type} />
                 </div>
                 <div>
                     author
-                    <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+                    <input name='author' value={author.value} onChange={author.onChange} type={author.type} />
                 </div>
                 <div>
                     url for more info
-                    <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+                    <input name='info' value={info.value} onChange={info.onChange} type={info.type} />
                 </div>
                 <button>create</button>
             </form>
@@ -107,7 +108,7 @@ const CreateNew = (props) => {
 
 }
 
-const Notif = ({notif}) => {
+const Notif = ({ notif }) => {
     if (!notif) {
         return null
     } else {
