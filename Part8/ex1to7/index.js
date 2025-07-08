c94c54379e59d7588bf72e6450ee4f12848df810
Uvarov1tte <ngqchia@gmail.com>
2025-07-08 12:12:26 +0300

    8.7

--- Part8/ex1to7/index.js
+++ Part8/ex1to7/index.js
@@ -136,6 +136,10 @@ const typeDefs = `
             name: String!
             born: Int
         ): Author
+        editAuthor(
+            name: String!
+            setBornTo: Int!
+        ): Author
     }
 `
 
@@ -206,6 +210,17 @@ const resolvers = {
             }
 
             return book
+        },
+
+        editAuthor: (root, args) => {
+            const author = authors.find(a => a.name === args.name)
+            if (!author) {
+                return null
+            }
+
+            const updatedAuthor = { ...author, born: args.setBornTo }
+            authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+            return updatedAuthor
         }
     }
 }
