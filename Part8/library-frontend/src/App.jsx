f1585ced6733923acf42bde97c6c5c090d9ff9df
Uvarov1tte <ngqchia@gmail.com>
2025-07-08 20:23:34 +0300

    8.8

--- Part8/library-frontend/src/App.jsx
+++ Part8/library-frontend/src/App.jsx
@@ -2,25 +2,36 @@ import { useState } from "react";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
+import { useQuery } from '@apollo/client'
+
+import { ALL_AUTHORS } from "./queries";
 
 const App = () => {
-  const [page, setPage] = useState("authors");
+	const [page, setPage] = useState("authors");
 
-  return (
-    <div>
-      <div>
-        <button onClick={() => setPage("authors")}>authors</button>
-        <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
-      </div>
+	const result = useQuery(ALL_AUTHORS)
 
-      <Authors show={page === "authors"} />
+	if (result.loading) {
+		return <div>loading...</div>
+	}
 
-      <Books show={page === "books"} />
+	const authors = result.data.allAuthors
 
-      <NewBook show={page === "add"} />
-    </div>
-  );
+	return (
+		<div>
+			<div>
+				<button onClick={() => setPage("authors")}>authors</button>
+				<button onClick={() => setPage("books")}>books</button>
+				<button onClick={() => setPage("add")}>add book</button>
+			</div>
+
+			<Authors show={page === "authors"} authors={authors} />
+
+			<Books show={page === "books"} />
+
+			<NewBook show={page === "add"} />
+		</div>
+	);
 };
 
 export default App;
