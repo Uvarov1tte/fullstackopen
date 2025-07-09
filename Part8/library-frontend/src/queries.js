4f25b7883d83780e17015ecd2568079cecfcbdd2
Uvarov1tte <ngqchia@gmail.com>
2025-07-09 22:53:29 +0300

    8,17

--- Part8/library-frontend/src/queries.js
+++ Part8/library-frontend/src/queries.js
@@ -14,7 +14,9 @@ export const ALL_BOOOKS = gql`
     query {
         allBooks {
             title
-            author
+            author {
+                name
+            }
             published
         }
     }
